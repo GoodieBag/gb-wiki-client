bab3d477cffa57428e7a9ca7c4888541bd836ebf
Krishanu Dey <krishanudey99@gmail.com>
2018-01-28 17:25:11 +0530

    Added basic layoutes for header, login and footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  isCollapsed: boolean = true;
 
+
+  toggleCollapse(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
   constructor() { }
 
   ngOnInit() {
